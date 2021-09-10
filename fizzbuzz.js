931658be6ab51695844503efe8ba175d3368cc32
JoseRodriguez-CR <josemariorodriguezc@gmail.com>
2021-09-10 15:40:43 -0600

    Updating File

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,7 +3,7 @@
     If the number is divisible by five 5 - Print "Buzz" instead of the number 
     If the number is divisible by three 3 and five 5 - Print "FizzBuzz" instead of the number 
 
-    Verify it witn number from 1 to 100
+    Verify it witn numbers from 1 to 100
 
     */
 
@@ -34,41 +34,4 @@ function fizzBuzzSequent (limit){
 }
 
 fizzBuzzSequent(100);
-console.log("______________________");
-
-
-
-/*
-Practicing modulus operator
-let a = 15%3;
-console.log(a);*/
-
-
-// First try to verify that we need to check both conditions first if dont  
-// the conditional will print just "Fizz" or "Buzz"
-
-/*
-for (let i = 1; i <=100; i++){
-    let mul3 = i%3;
-    if(mul3==0){
-        console.log("Fizz");
-    }
-    else{
-        let mul5 = i%5;
-        if(mul5==0){
-            console.log("Buzz");
-        }
-        else{
-            let mulThree = i%3;
-            let mulFive = i%5;
-            if(mulThree == 0  && mulFive == 0){
-                console.log("FizzBuzz");
-            }
-            else{
-                console.log(i);
-            }
-        }        
-    }
-    //console.log(i);
-}
-*/
+console.log("___________The_End___________");
